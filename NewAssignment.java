ce52a5ec3026e16ab721296923fe33a2de3507b5
rajavenky <ch.venkatesh3759@gmail.com>
2021-07-21 17:03:02 +0530

    added traingle file

---
+++ NewAssignment.java
@@ -0,0 +1,19 @@
+public class NewAssignment{
+              
+	public static void main(String[] args) {  
+						  
+	int a, r, v = 1;  
+
+	for (a = 1; a <= 4; a++)   
+	{  
+  
+	for (r = 1; r< a + 1; r++)   
+	{  
+  
+	System.out.print(v++ + " ");  
+	}  
+  
+	System.out.println();  
+	}  
+}  
+	}    
